80ceac48778505330f9218c0809e75ade3f7eb47
ayodelgirl <timimarvel@gmail.com>
2024-06-03 15:01:23 +0100

    correctionsss

--- src/_layouts/LandingPageLayout/New.jsx
+++ src/_layouts/LandingPageLayout/New.jsx
@@ -3,10 +3,10 @@ import React from "react";
 export default function New() {
   return (
     <div className="bg-[#E8FFDE] lg:h-[50px] flex gap-7 items-center p-2">
-      <button className="bg-[#69BD45] text-white p-1 px-5 text-[12px] rounded-full my-2 lg:ml-32 ">
+      <button className="bg-[#69BD45] text-white p-1 px-5 text-[12px] rounded-full my-1 lg:ml-32 ">
         What's new
       </button>
-      <p className="lg:mt-4 text-[12px] text-[#667085] p-2">
+      <p className="text-[12px] text-[#667085] p-2 items-center">
         Infinity Health offers a comprehensive suite of services designed to
         address challenges faced by healthcare businesses in Africa.
       </p>
